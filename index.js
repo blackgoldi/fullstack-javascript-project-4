32e11c95ab64160702a36941d186e8dd50b70842
Bulat Yarullin <goldik.black@yandex.ru>
2025-05-15 00:03:37 +0300

    fix again

--- index.js
+++ index.js
@@ -3,31 +3,23 @@ import axios from 'axios';
 import node from 'node:fs/promises';
 import formatter from './src/formatter.js';
 
-function createFile(path, response) {
-  return node.appendFile(path, response.data).then(() => {
-    console.log(path);
-    return response;
-  });
-}
-
 export default function loader(url, path) {
+  const filePath = `${path}/${formatter(url)}`;
+
   return axios
     .get(url)
     .then((response) => {
-      if (response.status === 200) {
-        node
-          .realpath(path)
-          .then(() => {
-            createFile(path + '/' + formatter(url), response);
-          })
-          .catch((error) => {
-            console.error('message:', error.message);
-            throw error;
-          });
+      if (response.status !== 200) {
+        throw new Error(`Unexpected status code: ${response.status}`);
       }
+      return node.appendFile(filePath, response.data);
+    })
+    .then(() => {
+      console.log(filePath);
+      return filePath;
     })
     .catch((error) => {
-      console.error('message:', error.message);
-      throw error;
+      console.error('Error:', error.message);
+      throw error; // Проброс ошибки для обработки снаружи
     });
 }
--- src/formatter.js
+++ src/formatter.js
@@ -1,5 +1,4 @@
 export default function formatter(url) {
   const myURL = new URL(url);
-  console.log(myURL.host + myURL?.pathname)
   return (myURL.host + myURL?.pathname).replaceAll(/[^a-zA-Z0-9]/g, '-') + '.html';
 }
