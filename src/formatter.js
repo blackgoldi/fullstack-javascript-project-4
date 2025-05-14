a46e7bf2e5393c6717946a0a130dcf9d2934e5b4
Bulat Yarullin <goldik.black@yandex.ru>
2025-05-14 23:07:54 +0300

    fix

--- src/formatter.js
+++ src/formatter.js
@@ -1,7 +1,5 @@
 export default function formatter(url) {
-  return (
-    url
-      .slice(url.search(/(?<=\/\/).+$/), url.length) //отрезаем протокол из результирующей строки
-      .replaceAll(/[^a-zA-Z0-9]/g, '-') + '.html' //заменяем все символы, кроме букв и символа точки на тире, добавляем расширение файла
-  );
+  const myURL = new URL(url);
+  console.log(myURL.host + myURL?.pathname)
+  return (myURL.host + myURL?.pathname).replaceAll(/[^a-zA-Z0-9]/g, '-') + '.html';
 }
