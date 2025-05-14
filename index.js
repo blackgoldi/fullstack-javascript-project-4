b1ab2c0fb8dad456f44b5cf129d388a26720efd7
Bulat Yarullin <goldik.black@yandex.ru>
2025-05-14 23:16:18 +0300

    123

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-import { Command } from 'commander';
+#!/usr/bin/env node
+import axios from 'axios';
+import node, { mkdir } from 'node:fs/promises';
+import formatter from './src/formatter.js';
 
-function loader() {
-  return true;
+function createFile(path, response) {
+  return node.appendFile(path, response.data).then(() => {
+    console.log(path);
+    return response;
+  });
+}
+
+export default function loader(url, path) {
+  return axios.get(url).then((response) => {
+    if (response.status === 200) {
+      node
+        .realpath(path)
+        .then(() => {
+          createFile(path + '/' + formatter(url), response);
+        })
+        .catch(() => {
+          mkdir(path, { recursive: true }).then(() => {
+            createFile(path + '/' + formatter(url), response);
+          });
+        });
+    }
+    return response;
+  });
 }
